c27d44f36e0f3286e2cf8e193290bc60b2ac9e58
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-06-09 13:59:20 +0200

    feat(jokesdb): add model

--- apps/jokedb/models.py
+++ apps/jokedb/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Joke(models.Model):
+    text = models.TextField(blank=False, null=False)
+    pub_date = models.DateTimeField('date published')
+    is_profane = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
