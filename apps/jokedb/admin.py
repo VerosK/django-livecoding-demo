11b36d04af3c7d70133651210a887f54cdefa4d2
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-06-09 14:09:11 +0200

    feat(jokesdb): add admin site

--- apps/jokedb/admin.py
+++ apps/jokedb/admin.py
@@ -1,5 +1,11 @@
+# apps/jokedb/admin.py
+
 from django.contrib import admin
 from .models import Joke
 
-# Register your models here.
-admin.site.register(Joke)
+class JokeAdmin(admin.ModelAdmin):
+    list_display = ('text', 'pub_date', 'is_profane')
+    list_filter = ['pub_date', 'is_profane']
+    search_fields = ['text']
+
+admin.site.register(Joke, JokeAdmin)
