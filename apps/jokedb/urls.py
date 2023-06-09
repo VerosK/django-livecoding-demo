fe6b2dc4ebb16790afa8a87bdeec384d2fcd31de
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-06-09 15:01:36 +0200

    feat(jokedb): add joke view

--- apps/jokedb/urls.py
+++ apps/jokedb/urls.py
@@ -1,8 +1,9 @@
 # apps/jokedb/urls.py
 
 from django.urls import path
-from .views import index
+from .views import index, show_joke
 
 urlpatterns = [
     path('', index),
+    path('joke/<int:joke_id>/', show_joke),
 ]
--- apps/jokedb/views.py
+++ apps/jokedb/views.py
@@ -1,5 +1,11 @@
 from django.http import HttpResponse
 
+from .models import Joke
 
 def index(request):
     return HttpResponse("Hello, world. You're at the jokes index.")
+
+
+def show_joke(request, joke_id):
+    the_joke = Joke.objects.get(pk=joke_id)
+    return HttpResponse(the_joke.text)
