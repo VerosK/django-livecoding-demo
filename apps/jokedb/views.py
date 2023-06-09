284fa87fed4f31948fbdc12b5386172afe5c1cbf
Věroš Kaplan <veros.kaplan@wftech.cz>
2023-06-09 15:27:01 +0200

    feat(jokedb): use templates

--- apps/jokedb/views.py
+++ apps/jokedb/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 from .models import Joke
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the jokes index.")
 
 
 def show_joke(request, joke_id):
-    the_joke = Joke.objects.get(pk=joke_id)
-    return HttpResponse(the_joke.text)
+    context = dict(
+        the_joke=Joke.objects.get(pk=joke_id)
+    )
+    return render(request, 'jokedb/joke_view.html', context)
